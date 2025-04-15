3e6f455515b97ae48c477bfb57f9ee30074173fa
n-stepanova <nstepanovaqa@gmail.com>
2025-04-15 20:01:11 +0300

    вынос значений в константы

--- level1/task1.js
+++ level1/task1.js
@@ -1,16 +1,19 @@
+const POSITIVE = "Положительное";
+const NEGATIVE = "Отрицательное";
+ 
 function showTaskAndNumber(number){
     console.log("Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.");
     console.log(`Дано число: ${number}`);
 }
 
 function checkPositiveOrNegative(number){
-    if(number>=0){
-        console.log("Положительное");
-    }else{
-        console.log("Отрицательное");
+    let numberType = POSITIVE;
+    if(number<0){
+        numberType=NEGATIVE;
     }
+    console.log(numberType);
 }
 
-let n = 3;
+let n = 0;
 showTaskAndNumber(n);
 checkPositiveOrNegative(n);
