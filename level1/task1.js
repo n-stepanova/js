2ec3c1eab4a7c16e4b401117412db728917842b3
n-stepanova <nstepanovaqa@gmail.com>
2025-04-15 19:56:35 +0300

    v2 - разделение кода на функции

--- level1/task1.js
+++ level1/task1.js
@@ -1,10 +1,16 @@
-console.log("Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.");
-
-let number = -19;
-
-console.log(`Дано число: ${number}`);
-if(number>=0){
-    console.log("Положительное");
-}else{
-    console.log("Отрицательное");
+function showTaskAndNumber(number){
+    console.log("Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.");
+    console.log(`Дано число: ${number}`);
 }
+
+function checkPositiveOrNegative(number){
+    if(number>=0){
+        console.log("Положительное");
+    }else{
+        console.log("Отрицательное");
+    }
+}
+
+let n = 3;
+showTaskAndNumber(n);
+checkPositiveOrNegative(n);
