964d9c3abcb50fced1053bbfb010daea7bd11983
n-stepanova <nstepanovaqa@gmail.com>
2025-05-21 19:42:29 +0300

    get string last character

--- level1/task2.js
+++ level1/task2.js
@@ -6,3 +6,12 @@ function getStringSize(str){
 
 console.log(getStringSize("hello"));
 console.log(getStringSize("abracadabra"));
+
+console.log("Дана строка. Выведите в консоль последний символ строки.");
+
+function getLastCharOfTheString(str){
+    return(str[str.length-1]);
+}
+
+console.log(getLastCharOfTheString("hello"));
+console.log(getLastCharOfTheString("abracadabra"));
