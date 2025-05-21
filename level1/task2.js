f56d47151568f7e661cc4f1eb7bdf1b3a1703bd8
n-stepanova <nstepanovaqa@gmail.com>
2025-05-21 19:37:08 +0300

    get string size

---
+++ level1/task2.js
@@ -0,0 +1,8 @@
+console.log("Дана строка. Выведите в консоль длину этой строки.");
+
+function getStringSize(str){
+    return(str.length);
+}
+
+console.log(getStringSize("hello"));
+console.log(getStringSize("abracadabra"));
